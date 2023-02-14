9ebcffe0af801b15f5d306ed94fa01facc9ebd76
Hector <hector@hector.com>
2023-02-14 09:02:49 -0600

    sbadmin installed

---
+++ app/Views/footer.php
@@ -0,0 +1,26 @@
+
+</div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2021</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="<?= base_url(); ?>/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="<?= base_url(); ?>/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url(); ?>/js/scripts.js"></script>
+    <script src="<?= base_url(); ?>/js/jquery.dataTables1.10.20.min.js"></script>
+    <script src="<?= base_url(); ?>/js/dataTables.bootstrap4.1.10.20.min.js"></script>
+    <script src="<?= base_url(); ?>/assets/demo/datatables-demo.js"></script>
+</body>
+
+</html>
