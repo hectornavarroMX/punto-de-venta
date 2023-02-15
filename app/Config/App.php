6e88fdce4ee7933fc7679faddcf9efe52eb0409e
Hector <hector@hector.com>
2023-02-14 23:51:23 -0600

    sbadmin2 blue

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/pos/public/';
+	public $baseURL = 'http://192.168.15.65/pos/public/';
 
 	/*
 	|--------------------------------------------------------------------------
